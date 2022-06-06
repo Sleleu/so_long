8e3934feef2fab034816f9b15000157c23144bd6
Sleleu <93100775+Sleleu@users.noreply.github.com>
2022-06-06 20:35:24 +0200

    Update ft_printf.c

--- includes/ft_printf/ft_printf.c
+++ includes/ft_printf/ft_printf.c
@@ -40,34 +40,3 @@ int	ft_printf(const char *format, ...)
 	va_end(args);
 	return (len);
 }
-
-/*
-int	main(void)
-{
-
-	char	*tab = "test";
-	char	*vide = "";
-	char	*null = NULL;
-        printf("char %c int %d str %s uint %u hex %x pointeur %p\n",
-       	'A', 456, "test", -123456, 1245, &tab);
-	ft_printf("char %c int %d str %s uint %u hex %x pointeur %p\n",
-       	'A', 456, "test", -123456, 1245, &tab);
-
-	printf("test 2 : str vide %s\n", vide);
-        ft_printf("test 2 : str vide %s\n", vide);
-	ft_printf("%p\n", null);
-	printf("%p\n", null);
-
-
-	//printf(" %c ", '0');
-	//ft_printf(" %c ", '0');
-
-	//int	i = 0;
-	//i = ft_printf("%d, %d", -10, 20);
-	//ft_printf("\n%d", i);
-
-	ft_printf("%");
-	//printf("%");
-	return (0);
-	
-}*/
